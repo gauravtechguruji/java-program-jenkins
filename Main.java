a6c53ad3cd5374805ff266d553278cb1e3d6a3a7
Shikhil Saxena <shikhilsaxena@cdacnoida.in>
2025-01-23 13:29:42 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main{
+    public static void main(String[] args) {
+        
+        System.out.println("This is my java program to test on jenkins");
+        System.out.println("Hello world");
+    }
+}
